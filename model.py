fafa84095f66948ada4d8678fe727a34979a1447
Tine Fabiani <fabijot@gmail.com>
2021-09-05 21:56:15 +0200

    dodal prijavo in registracijo

--- model.py
+++ model.py
@@ -2,6 +2,64 @@ from datetime import date
 import datetime
 import json
 
+
+def zasifriraj_geslo(geslo_v_cistopisu):
+    zasifrirano_geslo = "xxx" + geslo_v_cistopisu[::-1] + "xxx"
+    return zasifrirano_geslo
+
+class Uporabnik:
+    def __init__(self,uporabnisko_ime,zasifrirano_geslo,model):
+        self.uporabnisko_ime = uporabnisko_ime
+        self.zasifrirano_geslo = zasifrirano_geslo
+        self.model = model
+
+    @staticmethod
+    def registracija(uporabnisko_ime, geslo_v_cistopisu):
+        if Uporabnik.iz_datoteke(uporabnisko_ime) is not None:
+            raise ValueError("Uporabniško ime že obstaja")
+        else:
+            zasifrirano_geslo = zasifriraj_geslo(geslo_v_cistopisu)
+            uporabnik = Uporabnik(uporabnisko_ime, zasifrirano_geslo, Model())
+            uporabnik.v_datoteko()
+            return uporabnik
+
+    def v_slovar(self):
+        return {
+            "uporabnisko_ime": self.uporabnisko_ime,
+            "zasifrirano_geslo": self.zasifrirano_geslo,
+            "model": self.model.v_slovar()
+        }
+    def v_datoteko(self):
+        with open(Uporabnik.ime_uporabnikove_datoteke(self.uporabnisko_ime),"w") as datoteka:
+            json.dump(self.v_slovar(),datoteka,ensure_ascii=False, indent=4)
+
+    def preveri_geslo(self,geslo_v_cistopisu):
+        return self.zasifrirano_geslo == zasifriraj_geslo(geslo_v_cistopisu)
+
+    def nastavi_geslo(self,geslo_v_cistopisu):
+        self.zasifrirano_geslo = zasifriraj_geslo(geslo_v_cistopisu)
+
+    
+    @staticmethod
+    def ime_uporabnikove_datoteke(uporabnisko_ime):
+        return f"{uporabnisko_ime}.json"
+
+    @staticmethod
+    def iz_slovarja(slovar):
+        uporabnisko_ime = slovar["uporabnisko_ime"]
+        geslo = slovar["zasifrirano_geslo"]
+        model = Model.iz_slovarja(slovar["model"])
+        return Uporabnik(uporabnisko_ime,geslo,model)
+
+    @staticmethod
+    def iz_datoteke(uporabnisko_ime):
+        try:
+            with open(Uporabnik.ime_uporabnikove_datoteke(uporabnisko_ime)) as datoteka:
+                slovar = json.load(datoteka)
+                return Uporabnik.iz_slovarja(slovar)
+        except FileNotFoundError:
+            return None
+
 class Model:
     def __init__(self):
         self.teme = []
@@ -34,6 +92,7 @@ class Model:
             slovar = json.load(dat)
             return Model.iz_slovarja(slovar)
 
+    
 class Tema:
     def __init__(self,ime):
         self.ime = ime
@@ -55,6 +114,15 @@ class Tema:
             Viri.iz_slovarja(sl_virov) for sl_virov in slovar["viri"]
         ]
         return tema
+
+    def preveri_podatke_novega_vira(self, vir):
+        napake = {}
+        if not vir:
+            napake["vir"] = "vir mora biti neprazen."
+        for vir in self.viri:
+            if vir.ime == vir:
+                napake["vir"] = "vir že obstaja."
+        return napake
 
 class Viri:
     def __init__(self,ime,opis,datum = date.today()):
--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,75 +1,217 @@
 import bottle
-from os import error
+from os import error, path
 from model import Model
 from model import Tema
 from model import Viri
 from model import QR_kodirnik
+from model import Uporabnik
 import qrcode
 
 IME_DATOTEKE = "stanje.json"
-try:
-    moj_model = Model.preberi_iz_datoteke(IME_DATOTEKE)
-except FileNotFoundError:
-    moj_model = Model()
+PISKOTEK_UPORABNISKO_IME = "uporabnisko_ime"
+SKRIVNOST = "to bo pa tezko"
 
+
+
+def shrani_stanje(uporabnik):
+    uporabnik.shrani_stanje()
+
+
+def trenutni_uporabnik():
+    uporabnisko_ime = bottle.request.get_cookie(PISKOTEK_UPORABNISKO_IME, secret=SKRIVNOST)
+    if uporabnisko_ime:
+        return podatki_uporabnika(uporabnisko_ime)         
+    else:
+        bottle.redirect("/prijava/")
+
+def podatki_uporabnika(uporabnisko_ime):
+    try:
+        return Uporabnik.iz_datoteke(uporabnisko_ime)
+    except FileNotFoundError:
+        bottle.redirect("prijava/")
+            
 
 @bottle.get("/")
+def zacetna_stran():
+    bottle.redirect("/osnovna_stran/")
+
+
+@bottle.get("/osnovna_stran/")
 def osnovna_stran():
-    return bottle.template("domaca.html")
+    uporabnik = trenutni_uporabnik()
+    return bottle.template("domaca.html",uporabnik=uporabnik)
+
+@bottle.get("/registracija/")
+def registracija_get():
+    return bottle.template("registracija.html", napaka=None)
+
+
+@bottle.post("/registracija/")
+def registracija_post():
+    uporabnisko_ime = bottle.request.forms.getunicode("uporabnisko_ime")
+    geslo_v_cistopisu = bottle.request.forms.getunicode("geslo")
+    if not uporabnisko_ime:
+        return bottle.template("registracija.html", napaka="Vnesi uporabniško ime!")
+    try:
+        Uporabnik.registracija(uporabnisko_ime, geslo_v_cistopisu)
+        bottle.response.set_cookie(
+            PISKOTEK_UPORABNISKO_IME, uporabnisko_ime, path="/", secret=SKRIVNOST
+        )
+        bottle.redirect("/")
+    except ValueError as e:
+        return bottle.template(
+            "registracija.html", napaka=e.args[0]
+        )
+
+@bottle.get("/prijava/")
+def prijava_get():
+    return bottle.template("prijava.html", napaka = None)
+
+
+
+@bottle.post("/prijava/")
+def prijava_post():
+    geslo_v_cistopisu = bottle.request.forms.getunicode("geslo")
+    uporabnisko = bottle.request.forms.getunicode("uporabnisko_ime")
+    if uporabnisko:
+        uporabnik = podatki_uporabnika(uporabnisko)
+        if uporabnik.preveri_geslo(geslo_v_cistopisu):
+            bottle.response.set_cookie(PISKOTEK_UPORABNISKO_IME, uporabnisko, path="/", secret=SKRIVNOST)
+            bottle.redirect("/")
+        else:
+           return bottle.template("prijava.html", napaka = "Podatki za prijavo so napačni!") 
+    else:
+        return bottle.template("prijava.html", napaka = "Vnesi uporabniško ime")
+
+@bottle.post("/odjava/")
+def odjava():
+    bottle.response.delete_cookie(PISKOTEK_UPORABNISKO_IME, path="/")
+    bottle.response("/")        
 
 @bottle.get("/prikazi_teme/")
 def prikazi_teme():
+    uporabnik = trenutni_uporabnik()
     return bottle.template(
         "teme.html",
-        t = moj_model.teme
+        t = uporabnik.model.teme
     )
 
 
 @bottle.post("/dodaj_temo/")
 def dodaj_temo():
+    uporabnik = trenutni_uporabnik()
     ime = bottle.request.forms.getunicode("ime")
     tema = Tema(ime)
-    moj_model.dodaj_temo(tema)
-    moj_model.shrani_v_datoteko(IME_DATOTEKE)
+    uporabnik.model.dodaj_temo(tema)
+    uporabnik.v_datoteko()
     bottle.redirect("/prikazi_teme/")
 
 @bottle.post("/odstrani_temo/")
 def odstrani_temo():
+    uporabnik = trenutni_uporabnik()
     indeks = bottle.request.forms.getunicode("indeks")
-    tema = moj_model.teme[int(indeks)]
-    moj_model.izbriši_temo(tema)
-    moj_model.shrani_v_datoteko(IME_DATOTEKE)
-    bottle.redirect("/")
+    tema = uporabnik.model.teme[int(indeks)]
+    uporabnik.model.izbriši_temo(tema)
+    uporabnik.v_datoteko()
+    bottle.redirect("/prikazi_teme/")
 
 @bottle.get("/poglej_vire/")
 def poglej_vire():
+    uporabnik = trenutni_uporabnik()
     indeks = bottle.request.query.getunicode("indeks")
-    tema = moj_model.teme[int(indeks)]
+    tema = uporabnik.model.teme[int(indeks)]
     return bottle.template(
         "viri_teme.html",
         i = indeks,
+        ime = str(tema.ime).capitalize(),
         t = tema,
         viri = tema.viri
     )
-@bottle.post("/odstrani_vir/")
-def odstrani_vir():
-    indeks_teme = bottle.request.forms.getunicode("tema")
-    indeks =  bottle.request.forms.getunicode("indeks")
-    tema = moj_model.teme[int(indeks_teme)]
-    tema.odstrani_vir(tema.viri[int(indeks)])
-    moj_model.shrani_v_datoteko(IME_DATOTEKE)
-    bottle.redirect(f"/poglej_vire/?indeks={indeks_teme}")
+
+
+@bottle.get("/obrazec_za_dodajanje_vira/")
+def obrazec_za_dodajanje_vira():
+    uporabnik = trenutni_uporabnik()
+    indeks_teme = bottle.request.query.getunicode("tema")
+    tema = uporabnik.model.teme[int(indeks_teme)]
+    return bottle.template(
+        "obrazec_za_dodajanje_vira.html",
+        napake = {},
+        polja = {},
+        ime = str(tema.ime).capitalize(),
+        i = indeks_teme,
+        t = tema 
+    )
 
 @bottle.post("/dodaj_vir/")
 def dodaj_vir():
-    indeks_teme = bottle.request.forms.getunicode("tema")
-    ime = bottle.request.forms.getunicode("ime")
+    uporabnik = trenutni_uporabnik()
+    indeks_teme = bottle.request.forms.getunicode("indeks_teme")
+    vir = bottle.request.forms.getunicode("vir")
     opis = bottle.request.forms.getunicode("opis")
-    vir = Viri(ime,opis)
-    moj_model.teme[int(indeks_teme)].dodaj_vir(vir)
-    moj_model.shrani_v_datoteko(IME_DATOTEKE)
+    napake = {}
+    polja = {"vir": vir}
+    tema = uporabnik.model.teme[int(indeks_teme)]
+    napake = tema.preveri_podatke_novega_vira(vir)
+    if napake:
+        return bottle.template("obrazec_za_dodajanje_vira.html", napake=napake, polja=polja)
+    else:
+        v = Viri(vir,opis)
+        tema.dodaj_vir(v)
+        uporabnik.v_datoteko()
+        bottle.redirect(f"/poglej_vire/?indeks={indeks_teme}")
+
+
+@bottle.post("/odstrani_vir/")
+def odstrani_vir():
+    uporabnik = trenutni_uporabnik()
+    indeks_teme = bottle.request.forms.getunicode("tema")
+    indeks =  bottle.request.forms.getunicode("indeks")
+    tema = uporabnik.model.teme[int(indeks_teme)]
+    tema.odstrani_vir(tema.viri[int(indeks)])
+    uporabnik.v_datoteko()
     bottle.redirect(f"/poglej_vire/?indeks={indeks_teme}")
-    
+
+#@bottle.post("/dodaj_vir/")
+#def dodaj_vir():
+    #indeks_teme = bottle.request.forms.getunicode("tema")
+    #ime = bottle.request.forms.getunicode("ime")
+    #opis = bottle.request.forms.getunicode("opis")
+    #vir = Viri(ime,opis)
+    #moj_model.teme[int(indeks_teme)].dodaj_vir(vir)
+    #moj_model.shrani_v_datoteko(IME_DATOTEKE)
+    #bottle.redirect(f"/poglej_vire/?indeks={indeks_teme}")
+
+@bottle.get("/qr_kodirnik/")
+def zavetna_qr_kodirnik():
+    trenutni_uporabnik()
+    return bottle.template(
+        "QR-kodirnik.html"
+    )
+
+@bottle.post("/izdelaj_QR/")
+def izdelaj_QR():
+    vir = bottle.request.forms.getunicode("vir")
+    ime_slike = bottle.request.forms.getunicode("ime")
+    nova_koda = QR_kodirnik(vir,ime_slike)
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=5,
+    )
+    slika_ime = str(nova_koda.ime_slike).replace(" ", "_")
+    qr.add_data(f"{nova_koda.vir}")
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(f"{slika_ime}.png")
+    return bottle.template(
+        "koda.html",
+        s = slika_ime
+    )
+ 
+
+
 
 
 
